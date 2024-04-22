f24410cf755c67d43f50ee140daf7a75ff7c2b16
NaderQast <nader.qas@gmail.com>
2024-04-22 20:56:44 +0300

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='text-3xl font-bold'>
+      <p>Nader</p></div>
+    </>
+  )
+}
+
+export default App
