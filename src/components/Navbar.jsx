96c5306abfb5d8d58cc987eb410c3417ac00fd7a
NaderQast <nader.qas@gmail.com>
2024-04-26 18:25:10 +0300

    portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,21 @@
-import logo from '../assets/logo.png'
-import { FaLinkedinIn } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-import { FaTwitterSquare} from 'react-icons/fa'
-import React from 'react'
+import logo from "../assets/logo.png";
+import { FaLinkedinIn, FaTwitterSquare,FaGithub } from "react-icons/fa";;
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-centre justify-between py-6'>
-        <div className='flex flex-shrink-0 items-center '>
-            <img className='' src={logo} alt='logo'></img>
+    <nav className="mb-20 flex items-centre justify-between py-6">
+      <div className="flex flex-shrink-0 items-center ">
+        <img className="" src={logo} alt="logo"></img>
+      </div>
 
-        </div>
-        
-        <div className='flex m-8 gap-4 items-center justify-between py-6 text-4xl text-gray-300'>
-            <FaGithub />
-            <FaLinkedinIn />
-            <FaTwitterSquare />
-            </div>
+      <div className="flex m-8 gap-4 items-center justify-between py-6 text-4xl text-gray-300">
+        <FaGithub />
+        <FaLinkedinIn />
+        <FaTwitterSquare />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar 
+export default Navbar;
