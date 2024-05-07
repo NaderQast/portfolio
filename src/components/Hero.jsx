5e7064dc79925fa2fe98cc0fca6e108b1ed57fd6
NaderQast <nader.qas@gmail.com>
2024-05-07 17:54:32 +0300

    portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import {HERO_CONTENT} from "../constants/index";
-import p1 from '../assets/p1.jpg'
+import { HERO_CONTENT } from "../constants/index";
+import p1 from "../assets/p1.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
@@ -8,21 +18,40 @@ const Hero = () => {
       <div className=" flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-8 text-4xl font-thin lg-mt-16 lg:text-6xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-8 text-4xl font-thin lg-mt-16 lg:text-6xl"
+            >
               Nader Al-Quwatli
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span 
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible"className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Junior WEB Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tight">{HERO_CONTENT}</p>
+            </motion.span>
+            <motion.p 
+            variants={container(1)}
+            initial="hidden"
+            animate="visible"className="my-2 max-w-xl py-6 font-light tracking-tight">
+              {HERO_CONTENT}
+            </motion.p>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 lg:p-8">
-            <div className="flex justify-center">
-            <img className="rounded-lg bg-transparent mb-auto" src={p1} alt="" ></img>
-            </div>
-      
+        <div className="w-full lg:w-1/2 lg:motion.p-8">
+          <div className="flex justify-center">
+            <motion.img
+              initial={{x:100,opacity:0}}
+              animate={{x:0,opacity:1}}
+              transition={{duration: 1 ,delay:1.2}}
 
+              className="rounded-lg bg-transparent mb-auto"
+              src={p1}
+              alt=""
+            ></motion.img>
+          </div>
         </div>
       </div>
     </div>
